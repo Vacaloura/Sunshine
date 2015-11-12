4fe7773d4bc6c6a82d6f345575ba32644fe4b487
Vacaloura <manuelportalorenzo@gmail.com>
2015-11-12 19:00:03 +0100

    primer commit

--- app/src/main/java/com/tesege/sunshine/Main2Activity.java
+++ app/src/main/java/com/tesege/sunshine/Main2Activity.java
@@ -1,25 +1,26 @@
 package com.tesege.sunshine;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
+    public static String cadena=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.boton_magico);
+        setContentView(R.layout.activity_main2);
+        Button button = (Button) findViewById(R.boton);
+        EditText et = (EditText) findViewById(R.et);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
-                startActivity(intent);
+                cadena = et.getText();
             }
         });
     }
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
         return true;
     }
 
